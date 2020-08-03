6fb5808cb9c7f3e53f063e1a4bc5cf5077df38e4
Mahendra Bhama <maahibhama@gmail.com>
2020-08-03 11:28:08 +0530

    first commit

---
+++ src/screens/app/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import Root from '../root';
+
+import styles from './styles';
+
+const App = () => {
+  return (
+    <View style={styles.main}>
+      <Root></Root>
+    </View>
+  );
+};
+
+export default App;
---
+++ src/screens/root/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import styles from './styles';
+
+const Root = () => {
+  return <View style={styles.main}></View>;
+};
+
+export default Root;
