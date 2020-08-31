7fee7600a3b9a5a3247d1383c4f35a27232f8f0a
Mahendra Bhama <maahibhama@gmail.com>
2020-08-31 21:22:21 +0530

    create color and theme file

---
+++ src/theme/index.js
@@ -0,0 +1,8 @@
+import { Dimensions } from 'react-native';
+
+import Colors from './colors';
+
+const ScreenWidth = Dimensions.get('window').width;
+const ScreenHeight = Dimensions.get('window').height;
+
+export { Colors, ScreenWidth, ScreenHeight };
