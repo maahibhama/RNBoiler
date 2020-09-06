0da44df1d2541d7d50dacae12aa5d5965d49f0a9
Mahendra Bhama <maahibhama@gmail.com>
2020-09-06 14:45:30 +0530

    implement redux store

--- src/screens/app/index.js
+++ src/screens/app/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import { Provider } from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+
+import configureStore from '../../store';
 
 import Root from '../root';
 
 import styles from './styles';
 
+
+const {store, persistor} = configureStore();
+
 const App = () => {
   return (
-    <View style={styles.main}>
-      <Root></Root>
-    </View>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <View style={styles.main}>
+          <Root></Root>
+        </View>
+      </PersistGate>
+    </Provider>
   );
 };
 
