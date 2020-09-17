51c007eb7d29e2bc694260f7913d8e4946d240b9
Mahendra Bhama <maahibhama@gmail.com>
2020-09-17 11:37:39 +0530

    Added assets in assets file

--- src/constants/Assets.js
+++ src/constants/Assets.js
@@ -2,5 +2,15 @@ export const icons = {
     check: require('../assets/icons/check/check_icon.png'),
     delete: require('../assets/icons/delete/delete_icon.png'),
     redo: require('../assets/icons/redo/redo_icon.png'),
-    undo: require('../assets/icons/undo/undo_icon.png')
+    undo: require('../assets/icons/undo/undo_icon.png'),
+    lineColor: require('../assets/icons/lineColor/lineColor_icon.png'),
+    fillColor: require('../assets/icons/fillColor/fillColor_icon.png'),
+    menuColor: require('../assets/icons/menuColor/menuColor_icon.png'),
+    eraser: require('../assets/icons/eraser/eraser_icon.png'),
+    back: require('../assets/icons/back/back_icon.png')
+}
+
+export const images = {
+    pen: require('../assets/images/pen/pen.png'),
+    skech: require('../assets/images/skech/skech.png')
 }
